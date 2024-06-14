8be8e37b2152ac619e101db5a924f25b412ed4d6
7sogoo <gtsog0915@gmail.com>
2024-06-14 17:19:54 +0800

    changes

--- index.js
+++ index.js
@@ -1,79 +1,4 @@
-// const words = ["To do", "In progress", "Done", "Blocked"];
-// const colors = ["White", "Yellow", "Green", "Red"]
-// const cardContainer = document.getElementById("cardContainer");
-
-// for (let i = 0; i < words.length; i++) {
-//     const card = document.createElement("div");
-//     card.classList.add("card");
-//     cardContainer.appendChild(card);
-// }
-
-// const body = document.getElementsByTagName('body')[0];
-
-// const arr = [['Хорвоогийн ', 'хүчирхэг ', 'амьтан ', 'хүн'], ['Хосгүй ', 'хөгжилт ', 'ухаан ', 'сэтгэхүйт ', 'хүн']]
-
-
-// const createSpanElement = (text) => {
-//     const span = document.createElement('span')
-//     span.innerText = text
-//     return span;
-// } 
-
-// const render = (arr) => {
-//     const div = document.createElement('div')
-//     for(let i = 0; i < arr.length; i++){
-//         div.appendChild(createSpanElement(arr[i]))
-//     }
-//     body.appendChild(div)
-// }
-
-// for(let i = 0; i < arr.length; i++){
-//     render(arr[i])
-// }
-
-// const body = document.getElementsByTagName('body')[0];
-
-// const cardContainer = document.createElement('div');
-// cardContainer.classList.add('cardContainer');
-// body.appendChild(cardContainer);
-
-// const cardScroll = document.createElement('div')
-// cardScroll.classList.add('cardScroll')
-// cardContainer.appendChild(cardScroll)
-
-// const images = ["https://cdn.dribbble.com/userupload/14826460/file/still-6376723b9cc707c030bdf4280edf9b5d.png?format=webp&resize=400x300&vertical=center",
-// "https://cdn.dribbble.com/userupload/14844018/file/original-78bf484f7f07195e4f79a5ea19a4bf1f.png?format=webp&resize=400x300&vertical=center",
-// "https://cdn.dribbble.com/userupload/14735473/file/original-cb8d456e80037fda2b6faecda1f4f367.jpg?format=webp&resize=400x300&vertical=center",
-// "https://cdn.dribbble.com/userupload/14718368/file/original-34f0be3a0678ff3bde5d577f8d51cfb1.jpg?format=webp&resize=400x300&vertical=center",
-// "https://cdn.dribbble.com/userupload/14574088/file/original-0d0562eb9c4b9ee6e3548fb7e57cf901.jpg?format=webp&resize=400x300&vertical=center",
-// "https://cdn.dribbble.com/userupload/14853425/file/original-ccb9b5329563ca8167e3d3ec9b0200d4.jpg?format=webp&resize=400x300&vertical=center"
-// ]
-
-// const names = ["Web Design", "Print", "Product design", "Branding", "Mobile", "Illustration"]
-
-// const backgroundImage = () => {
-//     for (let i = 0; i < images.length; i++) {
-//         const cardContent = document.createElement('div');
-//         cardContent.classList.add('cardContent')
-//         cardScroll.appendChild(cardContent) 
-
-//         const span = document.createElement('span');
-//         span.classList.add('span');
-
-//         const card = document.createElement('div');
-//         card.classList.add("card");
-
-//         card.style.backgroundImage = `url(${images[i]})`;
-//         span.innerText = names[i];
-
-//         cardContent.appendChild(card);
-//         cardContent.appendChild(span);
-//     }
-// } 
-
-// backgroundImage()
-
-const body = document.getElementsByTagName('body')[0];
+const body = document.getElementsByTagName("body")[0];
 
 const editSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
 <g clip-path="url(#clip0_4089_57)">
@@ -95,84 +20,170 @@ const removeSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16
 <path d="M9.33334 7.33333V11.3333" stroke="#A30000" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
 
-const colors = ["White", "Yellow", "Green", "Red"];
-const titles = ["To do", "In progress", "Done", "Blocked"]
+const cardContainer = document.createElement("div");
+cardContainer.classList.add("cardContainer");
 
-const cardContainer = document.createElement('div');
-cardContainer.classList.add("cardContainer")
-
-const buttonDiv = document.createElement('div')
-buttonDiv.classList.add('buttonDiv')
-const addTaskButton = document.createElement('button');
-buttonDiv.appendChild(addTaskButton)
+const buttonDiv = document.createElement("div");
+buttonDiv.classList.add("buttonDiv");
+const addTaskButton = document.createElement("button");
+buttonDiv.appendChild(addTaskButton);
 addTaskButton.classList.add("addTaskButton");
-addTaskButton.innerText = "Add task"
+addTaskButton.innerText = "Add task";
 
-body.appendChild(buttonDiv)
+body.appendChild(buttonDiv);
 body.appendChild(cardContainer);
 
-const myList = ["",]
+const board = [
+  {
+    title: "Todo",
+    backgroundColor: "white",
+  },
+  {
+    title: "In progress",
+    backgroundColor: "yellow",
+  },
+  {
+    title: "Done",
+    backgroundColor: "green",
+  },
+  {
+    title: "Blocked",
+    backgroundColor: "red",
+  },
+];
 
-const createCard = () => {
-    for (let i = 0; i < colors.length; i++) {
-        const card = document.createElement('div')
-        card.classList.add('card');
-        cardContainer.appendChild(card);
-    
-        const cardHeader = document.createElement('div');
-        cardHeader.classList.add('cardHeader');
-        card.appendChild(cardHeader)
-    
-        const cardHeaderTitle = document.createElement('div');
-        cardHeaderTitle.classList.add('cardHeaderTitle');
-        cardHeader.appendChild(cardHeaderTitle);
-    
-        const dot = document.createElement('p');
-        dot.classList.add('dot');
-        cardHeaderTitle.appendChild(dot);
-    
-        const title = document.createElement('p');
-        title.classList.add('title');
-        cardHeaderTitle.appendChild(title);
-    
-        const number = document.createElement('p');
-        number.classList.add('number');
-        cardHeader.appendChild(number);
+const data = {
+  todo: [
+    {
+      description: "todo",
+      index: 0,
+    },
+    {
+      description: "l",
+    },
+  ],
 
-        const list = document.createElement('div')
-        list.classList.add('list');
-        card.appendChild(list);
-    
-        title.innerText = titles[i];
-        dot.style.backgroundColor = `${colors[i]}`
-        number.innerText = "5"
+  inProgress: [
+    {
+      description: "in progress",
+      index: 1,
+    },
+  ],
+
+  blocked: [
+    {
+      description: "blocked",
+      index: 2,
+    },
+  ],
+
+  done: [
+    {
+      description: "done",
+      index: 3,
+    },
+  ],
+};
+
+const elementCreate = (tagname, classname, innertext) => {
+    const newElement = document.createElement(tagname);
+    newElement.classList.add(classname);
+    newElement.innerText = innertext
+    return newElement;
+  };
+
+const createBoard = (title, countValue, color) => {
+  const board = elementCreate('div','board',"");
+  const header = elementCreate('div','header',"");
+  const list = elementCreate('div','list',"");
+  const headerTitle = elementCreate('div','headerTitle',"");
+  const circle = elementCreate('div','circle',"");
+  const headerText = elementCreate('p','headerText',`${title}`);
+  const count = elementCreate('div','count',`${countValue}`);
+
+  circle.style.backgroundColor = `${color}`;
+
+  headerTitle.appendChild(circle);
+  headerTitle.appendChild(headerText);
+  header.appendChild(headerTitle);
+  header.appendChild(count);
+  board.appendChild(header);
+  board.appendChild(list);
+  cardContainer.appendChild(board);
+};
+
+board.map((el) => {
+  createBoard(el.title, 5, el.backgroundColor);
+});
+
+const createTask = (desc, index) => {
+  const list = document.getElementsByClassName("list")[index];
+  const task = elementCreate('div','task',"");
+  const blackDot = elementCreate('p','blackDot',"");
+  const text = elementCreate('p','text',`${desc}`);
+  const edit = document.createElement("div");
+  const remove = document.createElement("div");
+
+  task.appendChild(blackDot);
+  task.appendChild(text);
+
+  task.appendChild(edit);
+  task.appendChild(remove);
+
+  edit.innerHTML = editSvg;
+  remove.innerHTML = removeSvg;
+
+  edit.addEventListener("click", () => editTask(task, text));
+  remove.addEventListener("click", () => removeTask(task));
+
+  list.appendChild(task);
+};
+
+const editTask = (task, text) => {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = text.textContent;
+  task.replaceChild(input, text);
+
+  input.addEventListener("blur", () => {
+    text.textContent = input.value;
+    task.replaceChild(text, input);
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      input.blur();
     }
-    
+  });
+
+  input.focus();
+};
+
+const removeTask = (task) => {
+  task.remove();
+};
+
+const addTask = (description, boardIndex = 0) => {
+  createTask(description, boardIndex);
+};
+
+const keys = Object.keys(data);
+keys.map((el, index) =>
+  data[el].map((task) => createTask(task.description, index))
+);
+
+const backdrop = document.getElementsByClassName('backdrop')[0];
+
+addTaskButton.onclick = () => {
+  backdrop.style.display = "flex"
 }
 
-createCard()
+const submit = document.getElementsByClassName('submit')[0]
 
-const createTask = () => {
-    const list = document.getElementsByClassName('list')[0];
-    const task = document.createElement('div');
-    task.classList.add('task');
-    const blackDot = document.createElement('p');
-    blackDot.classList.add('blackDot');
-    const text = document.createElement('p');
-    text.classList.add('text')
-    const edit = document.createElement('div');
-    const remove = document.createElement('div');
-
-    task.appendChild(blackDot);
-    task.appendChild(text);
-
-    task.appendChild(edit);
-    task.appendChild(remove);
-
-    text.innerText = "[loan-managament] - Add card component"
-    edit.innerHTML = editSvg;
-    remove.innerHTML = removeSvg;
-    list.appendChild(task);
+submit.onclick = () => {
+  const descrip = document.querySelector(".backdrop .status select").value;
+  const keys = Object.keys(data)
+  console.log(data);
+  backdrop.style.display = "none"
+  
 }
-
-createTask()
